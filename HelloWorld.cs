d3f74e8775fe84c5ef7ac4d9ce9d036304123b5e
Ariel Antequiera <arielantequiera@gmail.com>
2024-01-22 00:01:34 -0300

    First exercises

---
+++ HelloWorld.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("Hello World!");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
---
+++ Types.cs
@@ -0,0 +1,19 @@
+Console.WriteLine('b'); //Char
+Console.WriteLine("Hello World!"); //String
+Console.WriteLine(123); //Int
+Console.WriteLine(0.25F); //Float
+Console.WriteLine(2.625); // Double
+Console.WriteLine(12.39816m); // Decimal
+Console.WriteLine(true); // Boolean
+Console.WriteLine(false); // Boolean
+
+string firstName = "Bob";
+int integer = 3;
+double number = 34.4;
+
+Console.Write("Hello, ");
+Console.Write(firstName);
+Console.Write("! You have ");
+Console.Write(integer);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.WriteLine(number);
